8df37152fbf0b5a7027cadac60b2550b6b89202e
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-21 01:16:31 +0530

    Update Alerts.bengali.php

--- Themes/default/languages/Alerts.bengali.php
+++ Themes/default/languages/Alerts.bengali.php
@@ -2,46 +2,46 @@
 // Version: 2.1.3; Alerts
 
 // Load Alerts strings
-$txt['topic_na'] = '(private topic)';
-$txt['board_na'] = '(private board)';
+$txt['topic_na'] = '(ব্যক্তিগত বিষয়)';
+$txt['board_na'] = '(ব্যক্তিগত বোর্ড)';
 
-$txt['all_alerts'] = 'All alerts';
-$txt['mark_alerts_read'] = 'Mark read';
-$txt['alert_settings'] = 'Settings';
-$txt['alerts_no_unread'] = 'No unread alerts.';
+$txt['all_alerts'] = 'সমস্ত সতর্কতা';
+$txt['mark_alerts_read'] = 'পড়া হয়েছে';
+$txt['alert_settings'] = 'সেটিংস';
+$txt['alerts_no_unread'] = 'না পড়া সতর্কতা নেই.';
 
-$txt['alert_topic_reply'] = '{member_link} replied to the topic {topic_msg}';
-$txt['alert_topic_move'] = 'The topic {topic_msg} has been moved to {board_msg}';
-$txt['alert_topic_remove'] = 'The topic {topic_msg} has been deleted';
-$txt['alert_topic_unlock'] = 'The topic {topic_msg} has been unlocked';
-$txt['alert_topic_lock'] = 'The topic {topic_msg} has been locked';
-$txt['alert_topic_split'] = 'The topic {topic_msg} has been split';
-$txt['alert_topic_merge'] = 'One or more topics have been merged into {topic_msg}';
-$txt['alert_topic_sticky'] = 'The topic {topic_msg} has been stickied';
-$txt['alert_board_topic'] = '{member_link} started a new topic, {topic_msg}, in {board_msg}';
-$txt['alert_msg_unapproved_attachment'] = '{member_link} added a new unapproved attachment to {msg_msg} in {board_msg}';
-$txt['alert_topic_unapproved_topic'] = '{member_link} started a new unapproved topic, {topic_msg}, in {board_msg}';
-$txt['alert_msg_unapproved_post'] = '{member_link} made a new unapproved reply to {topic_msg} in {board_msg}';
-$txt['alert_topic_unapproved_reply'] = '{member_link} replied to your unapproved topic, {topic_msg}, in {board_msg}';
-$txt['alert_msg_quote'] = '{member_link} quoted you in {msg_msg}';
-$txt['alert_msg_mention'] = '{member_link} mentioned you in {msg_msg}';
-$txt['alert_msg_like'] = '{member_link} liked your post, {msg_msg}';
-$txt['alert_msg_report'] = '{member_link} reported the post {msg_msg}';
-$txt['alert_msg_report_reply'] = '{member_link} replied to the report about {msg_msg}';
-$txt['alert_member_report'] = '{member_link} reported the profile of {profile_msg}';
-$txt['alert_member_report_reply'] = '{member_link} replied to the report about the profile of {profile_msg}';
-$txt['alert_member_register_standard'] = '{member_link} just signed up';
-$txt['alert_member_register_approval'] = '{member_link} just signed up (account requires approval)';
-$txt['alert_member_register_activation'] = '{member_link} just signed up (account requires activation)';
-$txt['alert_member_group_request'] = '{member_link} has requested to join {group_name}';
-$txt['alert_groupr_approved'] = 'Your request to join {group_name} has been approved';
-$txt['alert_groupr_rejected'] = 'Your request to join {group_name} has been rejected{reason}';
-$txt['alert_event_new_guest'] = 'A new event, {event_title}, was added to the calendar';
-$txt['alert_event_new'] = '{member_link} added a new event, {event_title}, to the calendar';
-$txt['alert_paidsubs_expiring'] = 'Your {subscription_name} subscription will expire {end_time}';
-$txt['alert_paidsubs_expired'] = 'Your {subscription_name} subscription expired {end_time}';
-$txt['alert_member_buddy_request'] = '{member_link} added you as their buddy';
-$txt['alert_birthday_msg'] = '{happy_birthday}';
-$txt['alerts_none'] = 'You have no alerts.';
+$txt['alert_topic_reply'] = '{member_link} বিষয়টিতে উত্তর দেওয়া হয়েছে {topic_msg}';
+$txt['alert_topic_move'] = 'বিষয়টি {topic_msg} লাফ দিয়েছে {board_msg}';
+$txt['alert_topic_remove'] = 'বিষয়টি {topic_msg} মুছে দেওয়া হয়েছে';
+$txt['alert_topic_unlock'] = 'বিষয়টি {topic_msg} খুলে দেওয়া হয়েছে';
+$txt['alert_topic_lock'] = 'বিষয়টি {topic_msg} বন্ধ করে দেওয়া হয়েছে';
+$txt['alert_topic_split'] = 'বিষয়টি {topic_msg} ভাগ করে দেওয়া হয়েছে';
+$txt['alert_topic_merge'] = 'এটাতে এক বা তার বেশি বিষয় একত্রিত করা হয়েছে {topic_msg}';
+$txt['alert_topic_sticky'] = 'বিষয়টি {topic_msg} আঠালো করা হয়েছে';
+$txt['alert_board_topic'] = '{member_link} একটি নতুন বিষয় শুরু করা হয়েছে, {topic_msg}, ভিতরে {board_msg}';
+$txt['alert_msg_unapproved_attachment'] = '{member_link} একটি নতুন অননুমোদিত সংযুক্তি যোগ করা হয়েছে {msg_msg} ভিতরে {board_msg}';
+$txt['alert_topic_unapproved_topic'] = '{member_link} একটি নতুন অননুমোদিত বিষয় শুরু করা হয়েছে, {topic_msg}, ভিতরে {board_msg}';
+$txt['alert_msg_unapproved_post'] = '{member_link} একটি অননুমোদিত উত্তর যোগ করা হয়েছে {topic_msg} ভিতরে {board_msg}';
+$txt['alert_topic_unapproved_reply'] = '{member_link} অননুমোদিত বিষয়ে উত্তর দেওয়া হয়েছে, {topic_msg}, ভিতরে {board_msg}';
+$txt['alert_msg_quote'] = '{member_link} আপনি উদ্ধৃত {msg_msg}';
+$txt['alert_msg_mention'] = '{member_link} আপনাকে উল্লেখ করেছি {msg_msg}';
+$txt['alert_msg_like'] = '{member_link} আপনার পোস্ট পছন্দ করা হয়েছে, {msg_msg}';
+$txt['alert_msg_report'] = '{member_link} পোস্ট রিপোর্ট {msg_msg}';
+$txt['alert_msg_report_reply'] = '{member_link} রিপোর্ট সম্পর্কে উত্তর {msg_msg}';
+$txt['alert_member_report'] = '{member_link} এর প্রোফাইল রিপোর্ট করেছে {profile_msg}';
+$txt['alert_member_report_reply'] = '{member_link} এর প্রোফাইল সম্পর্কে রিপোর্টের উত্তর দিয়েছেন {profile_msg}';
+$txt['alert_member_register_standard'] = '{member_link} জাস্ট সাইন আপ';
+$txt['alert_member_register_approval'] = '{member_link} জাস্ট সাইন আপ (একাউন্ট র অনুমোদন প্রয়োজন)';
+$txt['alert_member_register_activation'] = '{member_link} জাস্ট সাইন আপ (একাউন্ট সক্রিয়করণ প্রয়োজন)';
+$txt['alert_member_group_request'] = '{member_link} যোগদানের অনুরোধ জানিয়েছেন {group_name}';
+$txt['alert_groupr_approved'] = 'আপনার যোগদানের অনুরোধ {group_name} অনুমোদিত হয়েছে';
+$txt['alert_groupr_rejected'] = 'আপনার যোগদানের অনুরোধ {group_name} প্রত্যাখ্যান করা হয়েছে{reason}';
+$txt['alert_event_new_guest'] = 'একটি নতুন ঘটনা, {event_title}, ক্যালেন্ডারে যোগ করা হয়েছে';
+$txt['alert_event_new'] = '{member_link} একটি নতুন ঘটনা যোগ হয়েছে, {event_title}, ক্যালেন্ডারে';
+$txt['alert_paidsubs_expiring'] = 'আপনার {subscription_name} সাবস্ক্রিপশনের মেয়াদ শেষ হবে {end_time}';
+$txt['alert_paidsubs_expired'] = 'আপনার {subscription_name} সাবস্ক্রিপশন শেষ হয়ে গেছে {end_time}';
+$txt['alert_member_buddy_request'] = '{member_link} আপনাকে তাদের বন্ধু হিসাবে যোগ করেছে';
+$txt['alert_birthday_msg'] = '{শুভ_জন্মদিন}';
+$txt['alerts_none'] = 'আপনার কোন সতর্কতা নেই.';
 
 ?>
