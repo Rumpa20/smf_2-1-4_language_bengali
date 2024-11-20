950ee20b62e5d9ca42e06ed77ca7fec3e8c36b0d
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-21 01:13:47 +0530

    Update Agreement.bengali.php

--- Themes/default/languages/Agreement.bengali.php
+++ Themes/default/languages/Agreement.bengali.php
@@ -1,15 +1,15 @@
 <?php
 // Version: 2.1.0; Agreement
 
-$txt['agreement'] = 'Registration Agreement';
-$txt['agreement_updated'] = 'Updated Registration Agreement';
-$txt['agreement_updated_desc'] = 'You must accept the terms of the registration agreement in order to continue using the forum.';
-$txt['agreement_accepted'] = 'You accepted the terms of this agreement on %1$s.';
-$txt['privacy_policy'] = 'Privacy Policy';
-$txt['privacy_policy_updated'] = 'Updated Privacy Policy';
-$txt['privacy_policy_updated_desc'] = 'You must accept the terms of the privacy policy in order to continue using the forum.';
-$txt['privacy_policy_accepted'] = 'You accepted the terms of this privacy policy on %1$s.';
-$txt['agreement_and_privacy_policy'] = 'Registration Agreement and Privacy Policy';
-$txt['agree'] = 'I Agree';
+$txt['agreement'] = 'নিবন্ধন চুক্তি';
+$txt['agreement_updated'] = 'নিবন্ধন চুক্তি আপডেট করা হয়েছে';
+$txt['agreement_updated_desc'] = 'ফোরাম ব্যবহার চালিয়ে যাওয়ার জন্য আপনাকে অবশ্যই নিবন্ধন চুক্তির শর্তাবলী মেনে নিতে হবে.';
+$txt['agreement_accepted'] = 'আপনি এই নিবন্ধন চুক্তি মেনে নিয়েছেন %1$s.';
+$txt['privacy_policy'] = 'গোপনীয় শর্তাবলী';
+$txt['privacy_policy_updated'] = 'গোপনীয় শর্তাবলী আপডেট করা হয়েছে';
+$txt['privacy_policy_updated_desc'] = 'ফোরাম ব্যবহার চালিয়ে যাওয়ার জন্য আপনাকে অবশ্যই নিবন্ধন চুক্তির শর্তাবলী মেনে নিতে হবে.';
+$txt['privacy_policy_accepted'] = 'আপনি গোপনীয় শর্তাবলী চুক্তি মেনে নিয়েছেন %1$s.';
+$txt['agreement_and_privacy_policy'] = 'নিবন্ধন চুক্তি এবং গোপনীয় শর্তাবলী';
+$txt['agree'] = 'আমি রাজি';
 
 ?>
