7f0f0232860edc142a8a08f9357c357e2875349f
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-22 08:08:54 +0530

    Add files via upload

--- Themes/default/languages/Agreement.bengali.php
+++ Themes/default/languages/Agreement.bengali.php
@@ -2,14 +2,14 @@
 // Version: 2.1.0; Agreement
 
 $txt['agreement'] = 'নিবন্ধন চুক্তি';
-$txt['agreement_updated'] = 'নিবন্ধন চুক্তি আপডেট করা হয়েছে';
-$txt['agreement_updated_desc'] = 'ফোরাম ব্যবহার চালিয়ে যাওয়ার জন্য আপনাকে অবশ্যই নিবন্ধন চুক্তির শর্তাবলী মেনে নিতে হবে.';
-$txt['agreement_accepted'] = 'আপনি এই নিবন্ধন চুক্তি মেনে নিয়েছেন %1$s.';
-$txt['privacy_policy'] = 'গোপনীয় শর্তাবলী';
-$txt['privacy_policy_updated'] = 'গোপনীয় শর্তাবলী আপডেট করা হয়েছে';
-$txt['privacy_policy_updated_desc'] = 'ফোরাম ব্যবহার চালিয়ে যাওয়ার জন্য আপনাকে অবশ্যই নিবন্ধন চুক্তির শর্তাবলী মেনে নিতে হবে.';
-$txt['privacy_policy_accepted'] = 'আপনি গোপনীয় শর্তাবলী চুক্তি মেনে নিয়েছেন %1$s.';
-$txt['agreement_and_privacy_policy'] = 'নিবন্ধন চুক্তি এবং গোপনীয় শর্তাবলী';
-$txt['agree'] = 'আমি রাজি';
+$txt['agreement_updated'] = 'আপডেট করা নিবন্ধন চুক্তি';
+$txt['agreement_updated_desc'] = 'ফোরাম ব্যবহার চালিয়ে যেতে নিবন্ধন চুক্তির শর্তাবলী মেনে নিতে হবে।';
+$txt['agreement_accepted'] = 'আপনি এই চুক্তির শর্তাবলী %1$s তারিখে মেনে নিয়েছেন।';
+$txt['privacy_policy'] = 'গোপনীয়তা নীতি';
+$txt['privacy_policy_updated'] = 'আপডেট করা গোপনীয়তা নীতি';
+$txt['privacy_policy_updated_desc'] = 'ফোরাম ব্যবহার চালিয়ে যেতে গোপনীয়তা নীতির শর্তাবলী মেনে নিতে হবে।';
+$txt['privacy_policy_accepted'] = 'আপনি এই গোপনীয়তা নীতির শর্তাবলী %1$s তারিখে মেনে নিয়েছেন।';
+$txt['agreement_and_privacy_policy'] = 'নিবন্ধন চুক্তি এবং গোপনীয়তা নীতি';
+$txt['agree'] = 'আমি মেনে নিচ্ছি';
 
 ?>
--- Themes/default/languages/Alerts.bengali.php
+++ Themes/default/languages/Alerts.bengali.php
@@ -2,46 +2,46 @@
 // Version: 2.1.3; Alerts
 
 // Load Alerts strings
-$txt['topic_na'] = '(ব্যক্তিগত বিষয়)';
-$txt['board_na'] = '(ব্যক্তিগত বোর্ড)';
+$txt['topic_na'] = '(গোপন বিষয়)';
+$txt['board_na'] = '(গোপন বোর্ড)';
 
 $txt['all_alerts'] = 'সমস্ত সতর্কতা';
-$txt['mark_alerts_read'] = 'পড়া হয়েছে';
+$txt['mark_alerts_read'] = 'পড়া চিহ্নিত করুন';
 $txt['alert_settings'] = 'সেটিংস';
-$txt['alerts_no_unread'] = 'না পড়া সতর্কতা নেই.';
+$txt['alerts_no_unread'] = 'কোনও অ-পড়া সতর্কতা নেই।';
 
-$txt['alert_topic_reply'] = '{member_link} বিষয়টিতে উত্তর দেওয়া হয়েছে {topic_msg}';
-$txt['alert_topic_move'] = 'বিষয়টি {topic_msg} লাফ দিয়েছে {board_msg}';
-$txt['alert_topic_remove'] = 'বিষয়টি {topic_msg} মুছে দেওয়া হয়েছে';
-$txt['alert_topic_unlock'] = 'বিষয়টি {topic_msg} খুলে দেওয়া হয়েছে';
-$txt['alert_topic_lock'] = 'বিষয়টি {topic_msg} বন্ধ করে দেওয়া হয়েছে';
-$txt['alert_topic_split'] = 'বিষয়টি {topic_msg} ভাগ করে দেওয়া হয়েছে';
-$txt['alert_topic_merge'] = 'এটাতে এক বা তার বেশি বিষয় একত্রিত করা হয়েছে {topic_msg}';
-$txt['alert_topic_sticky'] = 'বিষয়টি {topic_msg} আঠালো করা হয়েছে';
-$txt['alert_board_topic'] = '{member_link} একটি নতুন বিষয় শুরু করা হয়েছে, {topic_msg}, ভিতরে {board_msg}';
-$txt['alert_msg_unapproved_attachment'] = '{member_link} একটি নতুন অননুমোদিত সংযুক্তি যোগ করা হয়েছে {msg_msg} ভিতরে {board_msg}';
-$txt['alert_topic_unapproved_topic'] = '{member_link} একটি নতুন অননুমোদিত বিষয় শুরু করা হয়েছে, {topic_msg}, ভিতরে {board_msg}';
-$txt['alert_msg_unapproved_post'] = '{member_link} একটি অননুমোদিত উত্তর যোগ করা হয়েছে {topic_msg} ভিতরে {board_msg}';
-$txt['alert_topic_unapproved_reply'] = '{member_link} অননুমোদিত বিষয়ে উত্তর দেওয়া হয়েছে, {topic_msg}, ভিতরে {board_msg}';
-$txt['alert_msg_quote'] = '{member_link} আপনি উদ্ধৃত {msg_msg}';
-$txt['alert_msg_mention'] = '{member_link} আপনাকে উল্লেখ করেছি {msg_msg}';
-$txt['alert_msg_like'] = '{member_link} আপনার পোস্ট পছন্দ করা হয়েছে, {msg_msg}';
-$txt['alert_msg_report'] = '{member_link} পোস্ট রিপোর্ট {msg_msg}';
-$txt['alert_msg_report_reply'] = '{member_link} রিপোর্ট সম্পর্কে উত্তর {msg_msg}';
-$txt['alert_member_report'] = '{member_link} এর প্রোফাইল রিপোর্ট করেছে {profile_msg}';
-$txt['alert_member_report_reply'] = '{member_link} এর প্রোফাইল সম্পর্কে রিপোর্টের উত্তর দিয়েছেন {profile_msg}';
-$txt['alert_member_register_standard'] = '{member_link} জাস্ট সাইন আপ';
-$txt['alert_member_register_approval'] = '{member_link} জাস্ট সাইন আপ (একাউন্ট র অনুমোদন প্রয়োজন)';
-$txt['alert_member_register_activation'] = '{member_link} জাস্ট সাইন আপ (একাউন্ট সক্রিয়করণ প্রয়োজন)';
-$txt['alert_member_group_request'] = '{member_link} যোগদানের অনুরোধ জানিয়েছেন {group_name}';
-$txt['alert_groupr_approved'] = 'আপনার যোগদানের অনুরোধ {group_name} অনুমোদিত হয়েছে';
-$txt['alert_groupr_rejected'] = 'আপনার যোগদানের অনুরোধ {group_name} প্রত্যাখ্যান করা হয়েছে{reason}';
-$txt['alert_event_new_guest'] = 'একটি নতুন ঘটনা, {event_title}, ক্যালেন্ডারে যোগ করা হয়েছে';
-$txt['alert_event_new'] = '{member_link} একটি নতুন ঘটনা যোগ হয়েছে, {event_title}, ক্যালেন্ডারে';
-$txt['alert_paidsubs_expiring'] = 'আপনার {subscription_name} সাবস্ক্রিপশনের মেয়াদ শেষ হবে {end_time}';
-$txt['alert_paidsubs_expired'] = 'আপনার {subscription_name} সাবস্ক্রিপশন শেষ হয়ে গেছে {end_time}';
-$txt['alert_member_buddy_request'] = '{member_link} আপনাকে তাদের বন্ধু হিসাবে যোগ করেছে';
+$txt['alert_topic_reply'] = '{member_link} বিষয়টিতে উত্তর দিয়েছে {topic_msg}';
+$txt['alert_topic_move'] = 'বিষয়টি {topic_msg} স্থানান্তরিত হয়েছে {board_msg}';
+$txt['alert_topic_remove'] = 'বিষয়টি {topic_msg} মুছে ফেলা হয়েছে';
+$txt['alert_topic_unlock'] = 'বিষয়টি {topic_msg} আনলক করা হয়েছে';
+$txt['alert_topic_lock'] = 'বিষয়টি {topic_msg} লক করা হয়েছে';
+$txt['alert_topic_split'] = 'বিষয়টি {topic_msg} বিভক্ত করা হয়েছে';
+$txt['alert_topic_merge'] = 'এক বা একাধিক বিষয় {topic_msg} তে একত্রিত হয়েছে';
+$txt['alert_topic_sticky'] = 'বিষয়টি {topic_msg} স্টিকি করা হয়েছে';
+$txt['alert_board_topic'] = '{member_link} একটি নতুন বিষয় শুরু করেছে, {topic_msg}, {board_msg} তে';
+$txt['alert_msg_unapproved_attachment'] = '{member_link} {board_msg} তে {msg_msg} এ একটি নতুন অনুমোদিত সংযুক্তি যোগ করেছে';
+$txt['alert_topic_unapproved_topic'] = '{member_link} একটি নতুন অনুমোদিত বিষয় শুরু করেছে, {topic_msg}, {board_msg} তে';
+$txt['alert_msg_unapproved_post'] = '{member_link} {board_msg} তে {topic_msg} এ একটি নতুন অনুমোদিত উত্তর দিয়েছে';
+$txt['alert_topic_unapproved_reply'] = '{member_link} আপনার অনুমোদিত বিষয়, {topic_msg}, তে উত্তর দিয়েছে, {board_msg} তে';
+$txt['alert_msg_quote'] = '{member_link} আপনাকে {msg_msg} এ উদ্ধৃত করেছে';
+$txt['alert_msg_mention'] = '{member_link} আপনাকে {msg_msg} এ উল্লেখ করেছে';
+$txt['alert_msg_like'] = '{member_link} আপনার পোস্ট, {msg_msg} পছন্দ করেছে';
+$txt['alert_msg_report'] = '{member_link} পোস্ট {msg_msg} রিপোর্ট করেছে';
+$txt['alert_msg_report_reply'] = '{member_link} {msg_msg} সম্পর্কে রিপোর্টে উত্তর দিয়েছে';
+$txt['alert_member_report'] = '{member_link} {profile_msg} এর প্রোফাইল রিপোর্ট করেছে';
+$txt['alert_member_report_reply'] = '{member_link} {profile_msg} এর প্রোফাইল সম্পর্কে রিপোর্টে উত্তর দিয়েছে';
+$txt['alert_member_register_standard'] = '{member_link} সদ্য নিবন্ধন করেছে';
+$txt['alert_member_register_approval'] = '{member_link} সদ্য নিবন্ধন করেছে (অ্যাকাউন্টের অনুমোদন প্রয়োজন)';
+$txt['alert_member_register_activation'] = '{member_link} সদ্য নিবন্ধন করেছে (অ্যাকাউন্টের সক্রিয়করণ প্রয়োজন)';
+$txt['alert_member_group_request'] = '{member_link} {group_name} এ যোগদানের জন্য আবেদন করেছে';
+$txt['alert_groupr_approved'] = 'আপনার {group_name} এ যোগদানের আবেদন অনুমোদিত হয়েছে';
+$txt['alert_groupr_rejected'] = 'আপনার {group_name} এ যোগদানের আবেদন অস্বীকার করা হয়েছে{reason}';
+$txt['alert_event_new_guest'] = 'একটি নতুন ইভেন্ট, {event_title}, ক্যালেন্ডারে যোগ করা হয়েছে';
+$txt['alert_event_new'] = '{member_link} ক্যালেন্ডারে একটি নতুন ইভেন্ট, {event_title}, যোগ করেছে';
+$txt['alert_paidsubs_expiring'] = 'আপনার {subscription_name} সাবস্ক্রিপশন {end_time} এ শেষ হবে';
+$txt['alert_paidsubs_expired'] = 'আপনার {subscription_name} সাবস্ক্রিপশন {end_time} এ শেষ হয়েছে';
+$txt['alert_member_buddy_request'] = '{member_link} আপনাকে তাদের বন্ধু হিসেবে যোগ করেছে';
 $txt['alert_birthday_msg'] = '{happy_birthday}';
-$txt['alerts_none'] = 'আপনার কোন সতর্কতা নেই.';
+$txt['alerts_none'] = 'আপনার কোনো সতর্কতা নেই।';
 
 ?>
