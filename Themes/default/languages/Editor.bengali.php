f6218044b6a6f62c2e5df22b0e37161b0c53a98d
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-22 19:16:25 +0530

    Add files via upload

---
+++ Themes/default/languages/Editor.bengali.php
@@ -0,0 +1,65 @@
+<?php
+// Version: 2.1.0; Editor
+
+global $editortxt;
+
+$editortxt['bold'] = 'গা Bold ়';
+$editortxt['italic'] = 'পছন্দ';
+$editortxt['underline'] = 'আন্ডারলাইন';
+$editortxt['strikethrough'] = 'ক্রস আউট';
+$editortxt['subscript'] = 'সাবস্ক্রিপ্ট';
+$editortxt['superscript'] = 'সুপারস্ক্রিপ্ট';
+$editortxt['align_left'] = 'বাম দিকে সজ্জিত করুন';
+$editortxt['center'] = 'মধ্য';
+$editortxt['align_right'] = 'ডান দিকে সজ্জিত করুন';
+$editortxt['justify'] = 'সঠিকভাবে';
+$editortxt['font_name'] = 'ফন্টের নাম';
+$editortxt['font_size'] = 'ফন্টের আকার';
+$editortxt['font_color'] = 'ফন্টের রঙ';
+$editortxt['remove_formatting'] = 'ফরম্যাটিং মুছুন';
+$editortxt['cut'] = 'কাটুন';
+$editortxt['browser_no_cut'] = 'আপনার ব্রাউজার কাটার কমান্ড অনুমোদন করে না। দয়া করে কীবোর্ড শর্টকাট Ctrl/Cmd-X ব্যবহার করুন।';
+$editortxt['copy'] = 'কপি করুন';
+$editortxt['browser_no_copy'] = 'আপনার ব্রাউজার কপির কমান্ড অনুমোদন করে না। দয়া করে কীবোর্ড শর্টকাট Ctrl/Cmd-C ব্যবহার করুন।';
+$editortxt['paste'] = 'পেস্ট করুন';
+$editortxt['browser_no_paste'] = 'আপনার ব্রাউজার পেস্টের কমান্ড অনুমোদন করে না। দয়া করে কীবোর্ড শর্টকাট Ctrl/Cmd-V ব্যবহার করুন।';
+$editortxt['paste_your_text'] = 'নিচের বক্সে আপনার টেক্সট পেস্ট করুন:';
+$editortxt['paste_text'] = 'টেক্সট পেস্ট করুন';
+$editortxt['bullet_list'] = 'বুলেট তালিকা';
+$editortxt['numbered_list'] = 'সংখ্যাযুক্ত তালিকা';
+$editortxt['undo'] = 'পুনরুদ্ধার করুন';
+$editortxt['redo'] = 'পুনরায় করুন';
+$editortxt['rows'] = 'সারি:';
+$editortxt['cols'] = 'কলাম:';
+$editortxt['insert_table'] = 'একটি টেবিল সন্নিবেশ করুন';
+$editortxt['insert_horizontal_rule'] = 'একটি অনুভূমিক নিয়ম সন্নিবেশ করুন';
+$editortxt['code'] = 'কোড';
+$editortxt['insert_quote'] = 'একটি উদ্ধৃতি সন্নিবেশ করুন';
+$editortxt['width'] = 'প্রস্থ (ঐচ্ছিক):';
+$editortxt['height'] = 'উচ্চতা (ঐচ্ছিক):';
+$editortxt['insert_image'] = 'একটি ছবি সন্নিবেশ করুন';
+$editortxt['email'] = 'ই-মেইল:';
+$editortxt['insert_email'] = 'একটি ই-মেইল সন্নিবেশ করুন';
+$editortxt['url'] = 'URL:';
+$editortxt['insert_link'] = 'একটি লিঙ্ক সন্নিবেশ করুন';
+$editortxt['description'] = 'বিবরণ (ঐচ্ছিক)';
+$editortxt['unlink'] = 'লিঙ্ক মুছুন';
+$editortxt['more'] = 'আরও';
+$editortxt['close'] = 'বন্ধ করুন';
+$editortxt['insert_smiley'] = 'একটি স্মাইলি সন্নিবেশ করুন';
+$editortxt['video_url'] = 'ভিডিও URL:';
+$editortxt['insert'] = 'সন্নিবেশ করুন';
+$editortxt['insert_youtube_video'] = 'একটি YouTube ভিডিও সন্নিবেশ করুন';
+$editortxt['insert_current_date'] = 'বর্তমান তারিখ সন্নিবেশ করুন';
+$editortxt['insert_current_time'] = 'বর্তমান সময় সন্নিবেশ করুন';
+$editortxt['print'] = 'মুদ্রণ করুন';
+$editortxt['preformatted_text'] = 'পূর্বনির্ধারিত টেক্সট';
+$editortxt['view_source'] = 'সোর্স ভিউ টগল করুন';
+$editortxt['pre'] = 'পূর্বনির্ধারিত টেক্সট';
+$editortxt['flash'] = 'ফ্ল্যাশ সন্নিবেশ করুন';
+$editortxt['float_left'] = 'বাম দিকে ভাসান';
+$editortxt['float_right'] = 'ডান দিকে ভাসান';
+$editortxt['maximize'] = 'ম্যাক্সিমাইজ করুন';
+$editortxt['dateformat'] = 'মাস/দিন/বছর';
+
+?>
