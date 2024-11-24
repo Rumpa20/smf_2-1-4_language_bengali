b6eb1cd3cb807f1c99c0042ca062d41fee52cca8
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-25 00:40:09 +0530

    Add files via upload

---
+++ Themes/default/languages/Modlog.bengali.php
@@ -0,0 +1,120 @@
+<?php
+// Version: 2.1.0; Modlog
+
+$txt['modlog_date'] = 'তারিখ';
+$txt['modlog_member'] = 'সদস্য';
+$txt['modlog_position'] = 'পদ';
+$txt['modlog_action'] = 'কার্য';
+$txt['modlog_ip'] = 'আইপি';
+$txt['modlog_search_result'] = 'অনুসন্ধান ফলাফল';
+$txt['modlog_total_entries'] = 'মোট এন্ট্রি';
+$txt['modlog_ac_approve_topic'] = 'নিশ্চিত করা হয়েছে বিষয় &quot;{topic}&quot; দ্বারা &quot;{member}&quot;';
+$txt['modlog_ac_unapprove_topic'] = 'নিশ্চিত করা হয়নি বিষয় &quot;{topic}&quot; দ্বারা &quot;{member}&quot;';
+$txt['modlog_ac_approve'] = 'নিশ্চিত করা হয়েছে বার্তা &quot;{subject}&quot; মধ্যে &quot;{topic}&quot; দ্বারা &quot;{member}&quot;';
+$txt['modlog_ac_unapprove'] = 'নিশ্চিত করা হয়নি বার্তা &quot;{subject}&quot; মধ্যে &quot;{topic}&quot; দ্বারা &quot;{member}&quot;';
+$txt['modlog_ac_lock'] = 'লক করা হয়েছে &quot;{topic}&quot;';
+$txt['modlog_ac_warning'] = '{member} কে &quot;{message}&quot; এর জন্য সতর্ক করা হয়েছে';
+$txt['modlog_ac_unlock'] = 'আনলক করা হয়েছে &quot;{topic}&quot;';
+$txt['modlog_ac_sticky'] = 'স্টিকি করা হয়েছে &quot;{topic}&quot;';
+$txt['modlog_ac_unsticky'] = 'আনস্টিকি করা হয়েছে &quot;{topic}&quot;';
+$txt['modlog_ac_delete'] = 'মুছে ফেলা হয়েছে &quot;{subject}&quot; দ্বারা &quot;{member}&quot; থেকে &quot;{topic}&quot;';
+$txt['modlog_ac_delete_member'] = 'মুছে ফেলা হয়েছে সদস্য &quot;{name}&quot;';
+$txt['modlog_ac_remove'] = 'মুছে ফেলা হয়েছে বিষয় &quot;{topic}&quot; থেকে &quot;{board}&quot;';
+$txt['modlog_ac_modify'] = 'সম্পাদনা করা হয়েছে &quot;{message}&quot; দ্বারা &quot;{member}&quot;';
+$txt['modlog_ac_merge'] = 'বিষয়গুলিকে একত্রিত করা হয়েছে &quot;{topic}&quot; তৈরি করতে';
+$txt['modlog_ac_split'] = 'বিষয় &quot;{topic}&quot; বিভক্ত করা হয়েছে &quot;{new_topic}&quot; তৈরি করতে';
+$txt['modlog_ac_move'] = 'বিষয় &quot;{topic}&quot; স্থানান্তরিত করা হয়েছে &quot;{board_from}&quot; থেকে &quot;{board_to}&quot;';
+$txt['modlog_ac_profile'] = '&quot;{member}&quot; এর প্রোফাইল সম্পাদনা করুন';
+$txt['modlog_ac_pruned'] = '{days} দিনের পুরনো কিছু পোস্ট কাটা হয়েছে';
+$txt['modlog_ac_news'] = 'সংবাদ সম্পাদনা করা হয়েছে';
+$txt['modlog_ac_clearlog_moderate'] = 'মডারেশন লগ পরিষ্কার করা হয়েছে';
+$txt['modlog_ac_clearlog_admin'] = 'প্রশাসনিক লগ পরিষ্কার করা হয়েছে';
+$txt['modlog_enter_comment'] = 'মডারেশন মন্তব্য লিখুন';
+$txt['modlog_moderation_log'] = 'মডারেশন লগ';
+$txt['modlog_moderation_log_desc'] = 'নিচে সমস্ত মডারেশন কার্যক্রমের একটি তালিকা রয়েছে যা ফোরামের মডারেটরদের দ্বারা পরিচালিত হয়েছে।';
+$txt['modlog_no_entries_found'] = 'বর্তমানে কোনো মডারেশন লগ এন্ট্রি নেই।';
+$txt['modlog_remove'] = 'নির্বাচিত মুছে ফেলুন';
+$txt['modlog_removeall'] = 'লগ পরিষ্কার করুন';
+$txt['modlog_remove_selected_confirm'] = 'আপনি কি নিশ্চিত যে আপনি নির্বাচিত লগ এন্ট্রিগুলি মুছে ফেলতে চান?';
+$txt['modlog_remove_all_confirm'] = 'আপনি কি নিশ্চিত যে আপনি সম্পূর্ণ লগ পরিষ্কার করতে চান?';
+$txt['modlog_go'] = 'যাও';
+$txt['modlog_add'] = 'যোগ করুন';
+$txt['modlog_search'] = 'দ্রুত অনুসন্ধান';
+$txt['modlog_by'] = 'দ্বারা';
+$txt['modlog_id'] = '<em>মুছে ফেলা হয়েছে - আইডি:%1$d</em>';
+
+$txt['modlog_ac_add_warn_template'] = 'সতর্কতা টেমপ্লেট যোগ করা হয়েছে: &quot;{template}&quot;';
+$txt['modlog_ac_modify_warn_template'] = 'সতর্কতা টেমপ্লেট সম্পাদনা করা হয়েছে: &quot;{template}&quot;';
+$txt['modlog_ac_delete_warn_template'] = 'সতর্কতা টেমপ্লেট মুছে ফেলা হয়েছে: &quot;{template}&quot;';
+
+$txt['modlog_ac_ban'] = 'নিষেধাজ্ঞার ট্রিগার যোগ করা হয়েছে:';
+$txt['modlog_ac_ban_remove'] = 'নিষেধাজ্ঞার ট্রিগার মুছে ফেলা হয়েছে:';
+$txt['modlog_ac_ban_trigger_member'] = ' <em>সদস্য:</em> {member}';
+$txt['modlog_ac_ban_trigger_email'] = ' <em>ইমেইল:</em> {email}';
+$txt['modlog_ac_ban_trigger_ip_range'] = ' <em>আইপি:</em> {ip_range}';
+$txt['modlog_ac_ban_trigger_hostname'] = ' <em>হোস্টনেম:</em> {hostname}';
+
+$txt['modlog_admin_log'] = 'প্রশাসনিক লগ';
+$txt['modlog_admin_log_desc'] = 'নিচে আপনার ফোরামে লগ করা প্রশাসনিক কার্যক্রমের একটি তালিকা রয়েছে।';
+$txt['modlog_admin_log_no_entries_found'] = 'বর্তমানে কোনো প্রশাসনিক লগ এন্ট্রি নেই।';
+$txt['modlog_admin_log_gdpr_no_delete'] = '<strong>এছাড়াও লক্ষ্য করুন:</strong> নিবন্ধন চুক্তি এবং গোপনীয়তা নীতির আপডেটের জন্য এন্ট্রিগুলি এই লগ থেকে মুছে ফেলা যাবে না।';
+
+// প্রশাসক ধরনের স্ট্রিং।
+$txt['modlog_ac_upgrade'] = 'ফোরাম আপগ্রেড করা হয়েছে সংস্করণ {version}';
+$txt['modlog_ac_install'] = 'সংস্করণ {version} ইনস্টল করা হয়েছে';
+$txt['modlog_ac_add_board'] = 'নতুন বোর্ড যোগ করা হয়েছে: &quot;{board}&quot;';
+$txt['modlog_ac_edit_board'] = '&quot;{board}&quot; বোর্ড সম্পাদনা করা হয়েছে';
+$txt['modlog_ac_delete_board'] = '&quot;{boardname}&quot; বোর্ড মুছে ফেলা হয়েছে';
+$txt['modlog_ac_add_cat'] = 'নতুন বিভাগ যোগ করা হয়েছে, &quot;{catname}&quot;';
+$txt['modlog_ac_edit_cat'] = '&quot;{catname}&quot; বিভাগ সম্পাদনা করা হয়েছে';
+$txt['modlog_ac_delete_cat'] = '&quot;{catname}&quot; বিভাগ মুছে ফেলা হয়েছে';
+
+$txt['modlog_ac_delete_group'] = '&quot;{group}&quot; গ্রুপ মুছে ফেলা হয়েছে';
+$txt['modlog_ac_add_group'] = '&quot;{group}&quot; গ্রুপ যোগ করা হয়েছে';
+$txt['modlog_ac_edited_group'] = '&quot;{group}&quot; গ্রুপ সম্পাদনা করা হয়েছে';
+$txt['modlog_ac_added_to_group'] = '&quot;{member}&quot; কে &quot;{group}&quot; গ্রুপে যোগ করা হয়েছে';
+$txt['modlog_ac_removed_from_group'] = '&quot;{member}&quot; কে &quot;{group}&quot; গ্রুপ থেকে মুছে ফেলা হয়েছে';
+$txt['modlog_ac_removed_all_groups'] = '&quot;{member}&quot; কে সমস্ত গ্রুপ থেকে মুছে ফেলা হয়েছে';
+
+$txt['modlog_ac_agreement_updated'] = '{language} নিবন্ধন চুক্তি আপডেট করা হয়েছে';
+$txt['modlog_ac_policy_updated'] = '{language} গোপনীয়তা নীতি আপডেট করা হয়েছে';
+
+$txt['modlog_ac_remind_member'] = '&quot;{member}&quot; এর অ্যাকাউন্ট সক্রিয় করার জন্য একটি অনুস্মারক পাঠানো হয়েছে';
+$txt['modlog_ac_approve_member'] = '&quot;{member}&quot; এর অ্যাকাউন্ট অনুমোদন/সক্রিয় করা হয়েছে';
+$txt['modlog_ac_newsletter'] = 'নিউজলেটার পাঠানো হয়েছে';
+
+$txt['modlog_ac_install_package'] = 'নতুন প্যাকেজ ইনস্টল করা হয়েছে: &quot;{package}&quot;, সংস্করণ {version}';
+$txt['modlog_ac_upgrade_package'] = 'প্যাকেজ আপগ্রেড করা হয়েছে: &quot;{package}&quot; সংস্করণ {version}';
+$txt['modlog_ac_uninstall_package'] = 'প্যাকেজ আনইনস্টল করা হয়েছে: &quot;{package}&quot;, সংস্করণ {version}';
+
+// বিষয় পুনরুদ্ধার করুন।
+$txt['modlog_ac_restore_topic'] = '&quot;{board}&quot; থেকে &quot;{board_to}&quot; তে বিষয় &quot;{topic}&quot; পুনরুদ্ধার করা হয়েছে';
+$txt['modlog_ac_restore_posts'] = '&quot;{subject}&quot; থেকে পোস্টগুলি পুনরুদ্ধার করা হয়েছে বিষয় &quot;{topic}&quot; তে &quot;{board}&quot; বোর্ডে।';
+
+$txt['modlog_parameter_guest'] = '<em>অতিথি</em>';
+
+$txt['modlog_ac_approve_attach'] = '&quot;{filename}&quot; অনুমোদন করা হয়েছে &quot;{message}&quot; তে';
+$txt['modlog_ac_remove_attach'] = 'অনুমোদনহীন &quot;{filename}&quot; মুছে ফেলা হয়েছে &quot;{message}&quot; তে';
+
+// পোস্টের রিপোর্ট পরিচালনা করা
+$txt['modlog_report'] = 'রিপোর্ট';
+$txt['modlog_ac_close_report'] = '&quot;{message}&quot; তে {report} বন্ধ করা হয়েছে';
+$txt['modlog_ac_ignore_report'] = '&quot;{message}&quot; তে {report} উপেক্ষা করা হয়েছে';
+$txt['modlog_ac_open_report'] = '&quot;{message}&quot; তে {report} পুনরায় খোলা হয়েছে';
+$txt['modlog_ac_unignore_report'] = '&quot;{message}&quot; তে {report} এর উপেক্ষা প্রত্যাহার করা হয়েছে';
+
+// ব্যবহারকারীদের রিপোর্ট পরিচালনা করা
+$txt['modlog_ac_close_user_report'] = '{member} এর প্রোফাইলে {report} বন্ধ করা হয়েছে';
+$txt['modlog_ac_ignore_user_report'] = '{member} এর প্রোফাইলে {report} উপেক্ষা করা হয়েছে';
+$txt['modlog_ac_open_user_report'] = '{member} এর প্রোফাইলে {report} পুনরায় খোলা হয়েছে';
+$txt['modlog_ac_unignore_user_report'] = '{member} এর প্রোফাইলে {report} এর উপেক্ষা প্রত্যাহার করা হয়েছে';
+
+// ভোট পরিচালনা
+$txt['modlog_ac_add_poll'] = '&quot;{topic}&quot; তে একটি ভোট যোগ করা হয়েছে';
+$txt['modlog_ac_edit_poll'] = '&quot;{topic}&quot; তে ভোট সম্পাদনা করা হয়েছে';
+$txt['modlog_ac_lock_poll'] = '&quot;{topic}&quot; তে ভোটিং লক করা হয়েছে';
+$txt['modlog_ac_remove_poll'] = '&quot;{topic}&quot; থেকে ভোট মুছে ফেলা হয়েছে';
+$txt['modlog_ac_reset_poll'] = '&quot;{topic}&quot; তে ভোট পুনরায় সেট করা হয়েছে';
+$txt['modlog_ac_unlock_poll'] = '&quot;{topic}&quot; তে ভোটিং আনলক করা হয়েছে';
+
+?>
