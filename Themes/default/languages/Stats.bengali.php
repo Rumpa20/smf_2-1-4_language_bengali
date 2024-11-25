dbbc0a7935a12f1dc23e150e9e1f7c2723eadf6e
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-25 07:53:09 +0530

    Add files via upload

---
+++ Themes/default/languages/Stats.bengali.php
@@ -0,0 +1,44 @@
+<?php
+// Version: 2.1.0; Stats
+
+global $context;
+
+$txt['most_online'] = 'সর্বাধিক অনলাইন';
+
+$txt['stats_center'] = 'পরিসংখ্যান কেন্দ্র';
+$txt['general_stats'] = 'সাধারণ পরিসংখ্যান';
+$txt['top_posters'] = 'শীর্ষ 10 পোস্টার';
+$txt['top_boards'] = 'শীর্ষ 10 বোর্ড';
+$txt['forum_history'] = 'ফোরামের ইতিহাস (ফোরামের সময়ের অফসেট ব্যবহার করে)';
+$txt['stats_date'] = 'তারিখ (yyyy/mm/dd)';
+$txt['stats_new_topics'] = 'নতুন বিষয়বস্তু';
+$txt['stats_new_posts'] = 'নতুন পোস্ট';
+$txt['stats_new_members'] = 'নতুন সদস্য';
+$txt['page_views'] = 'পৃষ্ঠার দর্শন';
+$txt['top_topics_replies'] = 'শীর্ষ 10 বিষয়বস্তু (প্রতিবেদন দ্বারা)';
+$txt['top_topics_views'] = 'শীর্ষ 10 বিষয়বস্তু (দর্শনের দ্বারা)';
+$txt['yearly_summary'] = 'বার্ষিক সারসংক্ষেপ';
+$txt['top_starters'] = 'শীর্ষ বিষয়বস্তু শুরুকারীরা';
+$txt['top_time_online'] = 'সর্বাধিক সময় অনলাইনে';
+$txt['stats_more_detailed'] = 'আরও বিস্তারিত &raquo;';
+$txt['top_liked_messages'] = 'শীর্ষ পছন্দসই বার্তা';
+$txt['top_liked_users'] = 'শীর্ষ পছন্দসই ব্যবহারকারীরা';
+
+$txt['average_members'] = 'প্রতিদিনের গড় নিবন্ধন';
+$txt['average_posts'] = 'প্রতিদিনের গড় পোস্ট';
+$txt['average_topics'] = 'প্রতিদিনের গড় বিষয়বস্তু';
+$txt['average_online'] = 'প্রতিদিনের গড় অনলাইন';
+$txt['users_online'] = 'ব্যবহারকারীরা অনলাইনে';
+$txt['gender_stats'] = 'লিঙ্গ পরিসংখ্যান';
+$txt['users_online_today'] = 'আজ অনলাইনে';
+$txt['num_hits'] = 'মোট পৃষ্ঠার দর্শন';
+$txt['average_hits'] = 'প্রতিদিনের গড় পৃষ্ঠার দর্শন';
+
+$txt['ssi_comment'] = 'মন্তব্য';
+$txt['ssi_comments'] = 'মন্তব্যসমূহ';
+$txt['ssi_write_comment'] = 'মন্তব্য লিখুন';
+$txt['ssi_no_guests'] = 'আপনি এমন একটি বোর্ড নির্দিষ্ট করতে পারবেন না যা অতিথিদের অনুমতি দেয় না। আবার চেষ্টা করার আগে বোর্ড আইডি চেক করুন।';
+// argument(s): forum name
+$txt['xml_rss_desc'] = '%1$s থেকে লাইভ তথ্য';
+
+?>
