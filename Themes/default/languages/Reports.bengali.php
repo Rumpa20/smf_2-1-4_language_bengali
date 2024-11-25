6253108503a0a1a985d767d10561562a8d3acbdf
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-25 19:03:29 +0530

    Add files via upload

---
+++ Themes/default/languages/Reports.bengali.php
@@ -0,0 +1,160 @@
+<?php
+// Version: 2.1.0; Reports
+
+$txt['generate_reports_desc'] = 'এই বিভাগ থেকে আপনি আপনার ফোরামের প্রশাসনে সহায়তার জন্য বিভিন্ন রিপোর্ট তৈরি করতে পারেন। আপনার পছন্দের বিকল্পটি নির্বাচন করতে নীচের পদক্ষেপগুলি অনুসরণ করুন।';
+$txt['generate_reports_continue'] = 'অগ্রসর হোন';
+$txt['generate_reports_type'] = 'রিপোর্টের প্রকার নির্বাচন করুন';
+$txt['gr_type_boards'] = 'বোর্ড';
+$txt['gr_type_desc_boards'] = 'আপনার ফোরামের প্রতিটি বোর্ডের জন্য বর্তমান সেটিংস এবং অ্যাক্সেস স্তরগুলি প্রদর্শনকারী রিপোর্ট।';
+$txt['gr_type_board_perms'] = 'বোর্ড অনুমতি';
+$txt['gr_type_desc_board_perms'] = 'আপনার ফোরামের বিভিন্ন বোর্ডে প্রতিটি সদস্যগ্রুপের অনুমতি প্রদর্শনকারী রিপোর্ট তৈরি করুন।';
+$txt['gr_type_member_groups'] = 'সদস্যগ্রুপ';
+$txt['gr_type_desc_member_groups'] = 'আপনার ফোরামের প্রতিটি সদস্যগ্রুপের জন্য সেটিংস প্রদর্শনকারী রিপোর্ট।';
+$txt['gr_type_group_perms'] = 'গ্রুপ অনুমতি';
+$txt['gr_type_desc_group_perms'] = 'ফোরামে প্রতিটি সদস্যগ্রুপের অনুমতি সম্পর্কিত রিপোর্ট।';
+$txt['gr_type_staff'] = 'কর্মকর্তা';
+$txt['gr_type_desc_staff'] = 'এই রিপোর্টটি বর্তমানে ফোরামে কর্তৃত্বের পদে থাকা সকল সদস্যের সারসংক্ষেপ।';
+
+$txt['global_boards'] = 'গ্লোবাল বোর্ড';
+$txt['results'] = 'ফলাফল';
+
+// বোর্ড অনুমতি
+$txt['board_perms_permission'] = 'অনুমতি';
+$txt['board_perms_allow'] = 'অনুমতি দিন';
+$txt['board_perms_deny'] = 'অস্বীকার করুন';
+$txt['board_perms_name_announce_topic'] = 'বিজ্ঞপ্তি বিষয়';
+$txt['board_perms_name_approve_posts'] = 'পোস্ট অনুমোদন করুন';
+$txt['board_perms_name_delete_any'] = 'কোনও পোস্ট মুছে ফেলুন';
+$txt['board_perms_name_delete_own'] = 'নিজের পোস্ট মুছে ফেলুন';
+$txt['board_perms_name_delete_replies'] = 'নিজের বিষয়ের উত্তর মুছে ফেলুন';
+$txt['board_perms_name_lock_any'] = 'কোনও বিষয় লক করুন';
+$txt['board_perms_name_lock_own'] = 'নিজের বিষয় লক করুন';
+$txt['board_perms_name_make_sticky'] = 'বিষয়গুলি স্টিকি করুন';
+$txt['board_perms_name_merge_any'] = 'বিষয়গুলিকে একত্রিত করুন';
+$txt['board_perms_name_moderate_board'] = 'বোর্ড পরিচালনা করুন';
+$txt['board_perms_name_modify_any'] = 'কোনও পোস্ট পরিবর্তন করুন';
+$txt['board_perms_name_modify_own'] = 'নিজের পোস্ট পরিবর্তন করুন';
+$txt['board_perms_name_modify_replies'] = 'নিজের বিষয়ের উত্তর পরিবর্তন করুন';
+$txt['board_perms_name_move_any'] = 'কোনও বিষয় স্থানান্তর করুন';
+$txt['board_perms_name_move_own'] = 'নিজের বিষয় স্থানান্তর করুন';
+$txt['board_perms_name_poll_add_any'] = 'কোনও বিষয়ের জন্য ভোট যোগ করুন';
+$txt['board_perms_name_poll_add_own'] = 'নিজের বিষয়ের জন্য ভোট যোগ করুন';
+$txt['board_perms_name_poll_edit_any'] = 'কোনও ভোট সম্পাদনা করুন';
+$txt['board_perms_name_poll_edit_own'] = 'নিজের ভোট সম্পাদনা করুন';
+$txt['board_perms_name_poll_lock_any'] = 'কোনও ভোট লক করুন';
+$txt['board_perms_name_poll_lock_own'] = 'নিজের ভোট লক করুন';
+$txt['board_perms_name_poll_post'] = 'নতুন ভোট পোস্ট করুন';
+$txt['board_perms_name_poll_remove_any'] = 'কোনও ভোট মুছে ফেলুন';
+$txt['board_perms_name_poll_remove_own'] = 'নিজের ভোট মুছে ফেলুন';
+$txt['board_perms_name_poll_view'] = 'ভোট দেখুন';
+$txt['board_perms_name_poll_vote'] = 'ভোটে ভোট দিন';
+$txt['board_perms_name_post_attachment'] = 'সংযুক্তি পোস্ট করুন';
+$txt['board_perms_name_post_new'] = 'নতুন বিষয় পোস্ট করুন';
+$txt['board_perms_name_post_reply_any'] = 'কোনও বিষয়ের উত্তর পোস্ট করুন';
+$txt['board_perms_name_post_reply_own'] = 'নিজের বিষয়ের উত্তর পোস্ট করুন';
+$txt['board_perms_name_post_unapproved_attachments'] = 'অনুমোদিত নয় এমন সংযুক্তি পোস্ট করুন';
+$txt['board_perms_name_post_unapproved_topics'] = 'অনুমোদিত নয় এমন বিষয় পোস্ট করুন';
+$txt['board_perms_name_post_unapproved_replies_any'] = 'কোনও বিষয়ের জন্য অনুমোদিত নয় এমন উত্তর পোস্ট করুন';
+$txt['board_perms_name_post_unapproved_replies_own'] = 'নিজের বিষয়ের জন্য অনুমোদিত নয় এমন উত্তর পোস্ট করুন';
+$txt['board_perms_name_post_draft'] = 'পোস্টের খসড়া সংরক্ষণ করুন';
+$txt['board_perms_name_post_autosave_draft'] = 'পোস্টের খসড়া স্বয়ংক্রিয়ভাবে সংরক্ষণ করুন';
+$txt['board_perms_name_remove_any'] = 'কোনও বিষয় মুছে ফেলুন';
+$txt['board_perms_name_remove_own'] = 'নিজের বিষয় মুছে ফেলুন';
+$txt['board_perms_name_report_any'] = 'কোনও পোস্ট রিপোর্ট করুন';
+$txt['board_perms_name_split_any'] = 'কোনও বিষয় ভাগ করুন';
+$txt['board_perms_name_view_attachments'] = 'সংযুক্তি দেখুন';
+
+$txt['board_perms_group_no_polls'] = 'এই বোর্ডে ভোটের অনুমতি নেই';
+$txt['board_perms_group_reply_only'] = 'এই বোর্ডে শুধুমাত্র বিষয়ের উত্তর দেওয়ার অনুমতি আছে';
+$txt['board_perms_group_read_only'] = 'এই বোর্ডে পোস্ট করার অনুমতি নেই';
+
+// সদস্যগ্রুপের তথ্য!
+$txt['member_group_color'] = 'রঙ';
+$txt['member_group_min_posts'] = 'ন্যূনতম পোস্ট';
+$txt['member_group_max_messages'] = 'সর্বাধিক ব্যক্তিগত বার্তা';
+$txt['member_group_icons'] = 'আইকন';
+$txt['member_group_settings'] = 'সেটিংস';
+$txt['member_group_access'] = 'বোর্ড অ্যাক্সেস';
+
+// বোর্ডের তথ্য।
+$txt['none'] = 'কিছুই নেই';
+$txt['board_category'] = 'শ্রেণী';
+$txt['board_parent'] = 'মাতৃ বোর্ড';
+$txt['board_redirect'] = 'পুনর্নির্দেশ বোর্ড, পুনর্নির্দেশ করে';
+$txt['board_num_topics'] = 'বিষয়ের সংখ্যা';
+$txt['board_num_posts'] = 'পোস্টের সংখ্যা';
+$txt['board_count_posts'] = 'পোস্ট গণনা করুন';
+$txt['board_theme'] = 'বোর্ড থিম';
+$txt['board_override_theme'] = 'বোর্ড থিম জোরপূর্বক প্রয়োগ করুন';
+$txt['board_profile'] = 'অনুমতি প্রোফাইল';
+$txt['board_moderators'] = 'মডারেটর';
+$txt['board_moderator_groups'] = 'মডারেটর গ্রুপ';
+$txt['board_groups'] = 'অ্যাক্সেস সহ গ্রুপ';
+$txt['board_disallowed_groups'] = 'অ্যাক্সেস অস্বীকার করা গ্রুপ';
+
+// গ্রুপ অনুমতি।
+$txt['group_perms_name_access_mod_center'] = 'মডারেশন কেন্দ্র অ্যাক্সেস করুন';
+$txt['group_perms_name_admin_forum'] = 'ফোরাম প্রশাসন';
+$txt['group_perms_name_calendar_edit_any'] = 'কোনও ইভেন্ট সম্পাদনা করুন';
+$txt['group_perms_name_calendar_edit_own'] = 'নিজের ইভেন্ট সম্পাদনা করুন';
+$txt['group_perms_name_calendar_post'] = 'ইভেন্ট পোস্ট করুন';
+$txt['group_perms_name_calendar_view'] = 'ইভেন্ট দেখুন';
+$txt['group_perms_name_edit_news'] = 'ফোরামের খবর সম্পাদনা করুন';
+$txt['group_perms_name_issue_warning'] = 'সতর্কতা দিন';
+$txt['group_perms_name_manage_attachments'] = 'সংযুক্তি পরিচালনা করুন';
+$txt['group_perms_name_manage_bans'] = 'ব্যান পরিচালনা করুন';
+$txt['group_perms_name_manage_boards'] = 'বোর্ড পরিচালনা করুন';
+$txt['group_perms_name_manage_membergroups'] = 'সদস্যগ্রুপ পরিচালনা করুন';
+$txt['group_perms_name_manage_permissions'] = 'অনুমতি পরিচালনা করুন';
+$txt['group_perms_name_manage_smileys'] = 'স্মাইলি এবং বার্তা আইকন পরিচালনা করুন';
+$txt['group_perms_name_moderate_forum'] = 'ফোরাম পরিচালনা করুন';
+$txt['group_perms_name_pm_read'] = 'ব্যক্তিগত বার্তা পড়ুন';
+$txt['group_perms_name_pm_send'] = 'ব্যক্তিগত বার্তা পাঠান';
+$txt['group_perms_name_pm_draft'] = 'ব্যক্তিগত বার্তার খসড়া সংরক্ষণ করুন';
+$txt['group_perms_name_pm_autosave_draft'] = 'ব্যক্তিগত বার্তার খসড়া স্বয়ংক্রিয়ভাবে সংরক্ষণ করুন';
+$txt['group_perms_name_profile_extra_any'] = 'কোনও অতিরিক্ত অপশন সম্পাদনা করুন';
+$txt['group_perms_name_profile_extra_own'] = 'নিজের অতিরিক্ত অপশন সম্পাদনা করুন';
+$txt['group_perms_name_profile_identity_any'] = 'কোনও অ্যাকাউন্ট সেটিংস সম্পাদনা করুন';
+$txt['group_perms_name_profile_identity_own'] = 'নিজের অ্যাকাউন্ট সেটিংস সম্পাদনা করুন';
+$txt['group_perms_name_profile_server_avatar'] = 'একটি সার্ভার অ্যাভাটার নির্বাচন করুন';
+$txt['group_perms_name_profile_upload_avatar'] = 'একটি অ্যাভাটার আপলোড করুন';
+$txt['group_perms_name_profile_remote_avatar'] = 'একটি রিমোট অ্যাভাটার নির্বাচন করুন';
+$txt['group_perms_name_profile_remove_any'] = 'কোনও অ্যাকাউন্ট মুছে ফেলুন';
+$txt['group_perms_name_profile_remove_own'] = 'নিজের অ্যাকাউন্ট মুছে ফেলুন';
+$txt['group_perms_name_profile_title_any'] = 'কোনও কাস্টম শিরোনাম সম্পাদনা করুন';
+$txt['group_perms_name_profile_title_own'] = 'নিজের কাস্টম শিরোনাম সম্পাদনা করুন';
+$txt['group_perms_name_profile_view'] = 'কোনও ব্যবহারকারীর প্রোফাইল দেখুন';
+$txt['group_perms_name_search_posts'] = 'পোস্টের জন্য অনুসন্ধান করুন';
+$txt['group_perms_name_send_email_to_members'] = 'সদস্যদের জন্য একটি ফোরাম ইমেইল পাঠান';
+$txt['group_perms_name_view_mlist'] = 'সদস্য তালিকা দেখুন';
+$txt['group_perms_name_view_stats'] = 'ফোরামের পরিসংখ্যান দেখুন';
+$txt['group_perms_name_view_warning_any'] = 'কোনও ব্যক্তির সতর্কতা স্থিতি দেখুন';
+$txt['group_perms_name_view_warning_own'] = 'নিজের সতর্কতা স্থিতি দেখুন';
+$txt['group_perms_name_who_view'] = 'কেউ অনলাইনে আছে তা দেখুন';
+$txt['group_perms_name_profile_blurb_any'] = 'কোনও ব্যক্তির ব্যক্তিগত টেক্সট সম্পাদনা করুন';
+$txt['group_perms_name_profile_blurb_own'] = 'নিজের ব্যক্তিগত টেক্সট সম্পাদনা করুন';
+$txt['group_perms_name_profile_displayed_name_any'] = 'কোনও ব্যক্তির প্রদর্শিত নাম সম্পাদনা করুন';
+$txt['group_perms_name_profile_displayed_name_own'] = 'নিজের প্রদর্শিত নাম সম্পাদনা করুন';
+$txt['group_perms_name_profile_forum_any'] = 'কোনও ব্যক্তির ফোরাম প্রোফাইল সম্পাদনা করুন';
+$txt['group_perms_name_profile_forum_own'] = 'নিজের ফোরাম প্রোফাইল সম্পাদনা করুন';
+$txt['group_perms_name_profile_password_any'] = 'কোনও ব্যক্তির পাসওয়ার্ড পরিবর্তন করুন';
+$txt['group_perms_name_profile_password_own'] = 'নিজের পাসওয়ার্ড পরিবর্তন করুন';
+$txt['group_perms_name_profile_signature_any'] = 'কোনও ব্যক্তির atures সম্পাদনা করুন';
+$txt['group_perms_name_profile_signature_own'] = 'নিজের attributes সম্পাদনা করুন';
+$txt['group_perms_name_profile_website_any'] = 'কোনও ব্যক্তির ওয়েবসাইট ক্ষেত্র সম্পাদনা করুন';
+$txt['group_perms_name_profile_website_own'] = 'নিজের ওয়েবসাইট ক্ষেত্র সম্পাদনা করুন';
+$txt['group_perms_name_bbc'] = '[%1$s] BBCode ব্যবহার করুন';
+$txt['group_perms_name_likes_like'] = 'কোনও বিষয় পছন্দ করুন';
+$txt['group_perms_name_mention'] = 'অন্যদের @নাম দ্বারা উল্লেখ করুন';
+$txt['group_perms_name_report_user'] = 'ব্যবহারকারীদের রিপোর্ট করুন';
+$txt['group_perms_name_send_mail'] = 'সদস্যদের জন্য একটি ফোরাম ইমেইল পাঠান';
+
+$txt['report_error_too_many_staff'] = 'আপনার কাছে খুব বেশি কর্মী সদস্য রয়েছে। 300 জনের বেশি কর্মী সদস্যের সাথে রিপোর্ট কাজ করবে না।';
+$txt['report_staff_position'] = 'অনুযায়ী';
+$txt['report_staff_moderates'] = 'মডারেটস';
+$txt['report_staff_posts'] = 'পোস্ট';
+$txt['report_staff_last_login'] = 'শেষ লগইন';
+$txt['report_staff_all_boards'] = 'সমস্ত বোর্ড';
+$txt['report_staff_no_boards'] = 'কোন বোর্ড নেই';
+
+?>
