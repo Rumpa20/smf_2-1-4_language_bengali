eed3f4ff2acd5abfd61ee0594d9b1e92a472c31a
Rumpa20 <117419281+Rumpa20@users.noreply.github.com>
2024-11-25 18:46:38 +0530

    Add files via upload

---
+++ Themes/default/languages/Settings.bengali.php
@@ -0,0 +1,10 @@
+<?php
+// সংস্করণ: 2.1.0; সেটিংস
+
+global $settings;
+
+// যুক্তি(গুলি): সেটিংসে সংরক্ষিত images_url
+$txt['theme_thumbnail_href'] = '%1$s/thumbnail.png';
+$txt['theme_description'] = 'সিম্পল মেশিনস থেকে ডিফল্ট থিম।<br><br>লেখক: সিম্পল মেশিনস টিম';
+
+?>
